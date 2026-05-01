1fcd38267645b30a8518e00f0688467a36bb9088
sanxbot345 <sutarsih167@gmail.com>
2026-05-01 21:16:15 +0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const serviceAccount = require("./firebase.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
